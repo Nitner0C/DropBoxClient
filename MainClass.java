3068201daa7297e4e9b1d66c27bfc294b0f11530
NitneroC <corentin.foure@gmail.com>
2014-03-30 00:31:00 +0100

    commit of version 1.0

--- MainClass.java
+++ MainClass.java
@@ -9,7 +9,6 @@ import java.io.*;
 
 public class MainClass {
     public static void main(String[] args) throws IOException, DbxException{
-        System.out.println(System.getenv());
         MyWindow mW = new MyWindow("DropBox App", 600, 600);
     }
 }
